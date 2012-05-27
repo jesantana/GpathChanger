68e4aaefb6ab5e6047b00f0e36a6ae3210e14074
Julio Enrique Santana Lora <KIKI@KIKI-PC.(none)>
2012-05-28 00:10:30 +0200

    Cambios en lanzador

--- ejecutaGroovy.bat
+++ ejecutaGroovy.bat
@@ -1,10 +1,12 @@
 cls
 
-set path=D:\Instalaciones\groovy-1.8.4\bin;%path%
+set rutaFile="C:\Sanitas\bravoweb\Desarrollo\BWManGen\trunkFacelets\ViewController\public_html\jsp\generales01"
 
 
 
-groovy C:\Users\KIKI\workspace\GroovyTest\src\sanitas\GIdChanger.groovy "D:\Programas\Sanitas\DSS101414- BW Consola\ViewController\public_html\jsp" "Al" "Aceptar" "../sfc/img/bwAceptar.png" >D:\Programas\salida.txt
+set path=C:\Sanitas\datos_PC\cambiarids\groovy-1.8.4\bin;%path%
+set cpcontent= "C:\Sanitas\datos_PC\cambiarids\GroovyTest\src;C:\Sanitas\datos_PC\cambiarids\GroovyTest\GroovyTest\bin"
 
 
+call groovy  --classpath %cpcontent% "C:\Sanitas\datos_PC\cambiarids\GroovyTest\src\sanitas\markhand\Pruebas.groovy" %rutaFile%
 pause
